5f7a62499b41811f5c58b5b8259902d15b1bea4a
Hendric Eckelt <hendric.eckelt@gmail.com>
2024-12-17 20:18:46 +0100

    Initialize PrimeVue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,15 @@
 import './assets/main.css'
 import App from './App.vue'
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App)
+	.use(createPinia())
+	.use(PrimeVue, {
+		theme: {
+			preset: Aura,
+		},
+	})
+	.mount('#app')
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Button label="Verify" />
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
