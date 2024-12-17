1c66ede3e5254436a516546f134a4cb56a42e08c
Hendric Eckelt <Hendric.Eckelt@gmail.com>
2024-12-17 20:53:49 +0100

    Setup PrimeVue + UnoCSS (#12)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,23 @@
 import './assets/main.css'
+import 'virtual:uno.css'
 import App from './App.vue'
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App)
+	.use(createPinia())
+	.use(PrimeVue, {
+		theme: {
+			preset: Aura,
+			options: {
+				cssLayer: {
+					name: 'primevue',
+					order: 'unocss, primevue',
+				},
+				darkModeSelector: '.dark-mode',
+			},
+		},
+	})
+	.mount('#app')
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center">
+    <h1>Culinetics</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
